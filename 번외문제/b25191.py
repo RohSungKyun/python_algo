d94e78c38f06f355512052459c12a60d638511be
RohSungKyun <songkyun1108@naver.com>
2022-09-08 00:25:32 +0900

    bronze

--- 번외문제/b25191.py
+++ 번외문제/b25191.py
@@ -3,6 +3,13 @@ read = sys.stdin.readline
 
 n = int(read())
 # 콜라 2개 맥주 1개
-c, m = map(int, read().split())
+c, b = map(int, read().split())
+
+max_chicken = (c//2)+b
+
+if max_chicken>n:
+    print(n)
+else:
+    print(max_chicken)
 
 
