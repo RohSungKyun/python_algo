cde57cb8ffee817a2001ab123cd1ae5613ca2834
RohSungKyun <songkyun1108@naver.com>
2022-10-14 22:30:16 +0900

    greedy

--- 그리디/b11047.py
+++ 그리디/b11047.py
@@ -6,7 +6,7 @@ arr = []
 for i in range(1, n+1):
     arr.append(int(read())) # 동전의 종류를 순서대로 저장(오름차순으로 저장됨)
 
-arr.sort(reverse=True) # 내림차순(큰 금액부터 탐색)
+arr.sort(reverse=True) # 내림차순(큰 금액부터 탐색) 
 
 cnt = 0
 
