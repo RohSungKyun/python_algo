846fd767d0da822e99691db2f88506830fd6ef92
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-01-18 23:29:00 +0900

    devide and conquer b2630(1/5)

--- 분할정복/b2630.py
+++ 분할정복/b2630.py
@@ -6,3 +6,12 @@ cp = []
 for i in range(n):
     cp.append(list(map(int, read().split())))
 
+def solve(nbs, x, y):
+    global blue, white
+    if nbs == 1: # 분할하여 나온 값이 1일 경우 더해줌
+        if cp[x][y]==1:
+            blue+=1
+            return
+        else:
+            white+=1
+            return
