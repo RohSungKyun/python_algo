4027c8f11c8bbc436065f2a7faba22570675f13a
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-03-28 23:31:18 +0900

    0328 dp 1로만들기 에러

---
+++ 이코테/dp_1로만들기.py
@@ -0,0 +1,15 @@
+import sys
+
+read = sys.stdin.readline
+x = int(read())
+dp=[0]*30001
+for i in range(2, x-1):
+    dp[i] = dp[i-1]+1 # 1을 빼는 연산
+    if i%2 == 0:
+        dp[i] = min(dp[i], dp[i//2]+1)
+    if i%3 == 0:
+        dp[i] = min(dp[i], dp[i//3]+1)
+    if i%5 == 0:
+        dp[i] = min(dp[i], dp[i//5]+1)
+
+print(dp[x])
