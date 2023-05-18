26b9f2eb56bb12c9254f8791138d47c1c88f0d2e
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-05-18 23:51:46 +0900

    0518 동전 점화식

--- 다이나믹 프로그래밍/b2293.py
+++ 다이나믹 프로그래밍/b2293.py
@@ -1,4 +1,20 @@
+# 동전의 가치의 합이 k가 되도록 하는 모든 경우의 수
+# 동전의 재사용 가능
+# 
+
 import sys
-read = sys.stdin.realdine
+read = sys.stdin.readline
 
 n, k = map(int, read().split())
+dp = [0 for i in range(k+1)]
+dp[0] = 1 # k짜리 코인 1개만 이용하는 경우
+coin = []
+for i in range(n):
+    coin.append(int(read()))
+
+for i in coin:
+    for j in range(i, k+1):
+        dp[j] += dp[j-i]
+print(dp[k])
+
+
