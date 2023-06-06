be9a8482550be3ce4c133a768cd8a59248ce5bfb
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-06-06 21:57:52 +0900

    0606

---
+++ 다이나믹 프로그래밍/b9465.py
@@ -0,0 +1,26 @@
+import sys
+read = sys.stdin.readline
+
+t = int(read())
+
+for _ in range(t):
+    n = int(read())
+    dp = [[0]*(n) for _ in range(2)]
+
+    arr = [list(map(int, read().split())) for _ in range(2)]
+
+    # init dp table 
+    dp[0][0] = arr[0][0]
+    dp[1][0] = arr[1][0]
+    dp[0][1] = dp[1][0] + arr[0][1]
+    dp[1][1] = dp[0][0] + arr[1][1]
+
+
+    for i in range(2, n):
+        for j in range(2):
+
+            # the maximum value two spaces before
+            dp[j][i] = max(dp[0][i-2], dp[1][i-2]) + arr[j][i]
+            # the maximum value one sapce before
+            dp[j][i] = max(dp[j][i], dp[(j+1)%2][i-1] + arr[j][i])
+    print(max(dp[0][-1], dp[1][-1]))
