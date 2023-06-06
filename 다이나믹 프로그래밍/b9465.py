6744e8b023f4e6a299b4d26ad209ce9ac50a1b4a
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-06-06 22:13:35 +0900

    0606 스티커문제 런타임에러 해결

--- 다이나믹 프로그래밍/b9465.py
+++ 다이나믹 프로그래밍/b9465.py
@@ -9,13 +9,27 @@ for _ in range(t):
 
     arr = [list(map(int, read().split())) for _ in range(2)]
 
+
+    
+    # If the length is 1
+    dp[0][0] = arr[0][0]
+    dp[1][0] = arr[1][0]
+    if n == 1:
+        print(max(dp[0][0], dp[1][0]))
+        continue
+
+    # if the length is 2
+    dp[0][1] = arr[1][0] + arr[0][1]
+    dp[1][1] = arr[0][0] + arr[1][1]
+    if n == 2:
+        print(max(dp[0][1], dp[1][1]))
+        continue
+    
     # init dp table 
     dp[0][0] = arr[0][0]
     dp[1][0] = arr[1][0]
     dp[0][1] = dp[1][0] + arr[0][1]
     dp[1][1] = dp[0][0] + arr[1][1]
-
-
     for i in range(2, n):
         for j in range(2):
 
