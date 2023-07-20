14020adbc7324184e4221b0334465100286ad258
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-07-20 23:18:56 +0900

    0720 그림의 영역 개수 구하기 구현

---
+++ BFS/b1926.py
@@ -0,0 +1,36 @@
+# 그림 1로 되있는 부분이 그림의 영역이다.
+
+import sys
+from collections import deque
+read  = sys.stdin.readline
+
+n, m = map(int, read().split())
+dx, dy = [0, 1, -1, 0], [1, 0, 0, -1]
+graph = []
+visited = [[False]*m for _ in range(n)]
+
+
+for i in range(n):
+    tmp = list(map(int,read().split()))
+    graph.append(tmp)
+
+def bfs(a, b):
+    queue = deque()
+    queue.append([a, b])
+    visited[a][b] = True
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+            if 0<=nx<n and 0<=ny<m and graph[nx][ny] == 1 and visited[nx][ny] == False:
+                 queue.append([nx, ny])
+                 visited[nx][ny] = True
+cnt = 0
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1 and visited[i][j] == False:
+            bfs(i, j)
+            cnt+=1
+
+print(cnt)
--- dfs/b2644.py
+++ dfs/b2644.py
@@ -1,13 +1,16 @@
+# 촌수계산
 import sys
 read = sys.stdin.readline
 n = int(read())
 p1 , p2 = map(int, read().split())
 m = int(read())
 
-def bfs():
-    
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
 
-for i in range(m):
+for _ in range(m):
+    x, y = map(int, read().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
-    dfs(i)
-
+print(graph)
