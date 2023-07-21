22cd3d7f06b404eccd87c40b972997cfd1baac71
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-07-22 00:17:09 +0900

    0722 1926 런타임 에러 해결

--- BFS/b1926.py
+++ BFS/b1926.py
@@ -18,7 +18,7 @@ def bfs(a, b):
     queue = deque()
     queue.append([a, b])
     visited[a][b] = True
-    global area
+    area = 1
     while queue:
         x, y = queue.popleft()
         for i in range(4):
@@ -27,18 +27,22 @@ def bfs(a, b):
                 queue.append([nx, ny])
                 visited[nx][ny] = True
                 area+=1
-    print(area)
+    return area
+
 cnt = 0
-area=0
-max_area = 0
+area = 0
+area_list = []
 for i in range(n):
     for j in range(m):
         if graph[i][j] == 1 and visited[i][j] == False:
-            bfs(i, j)
             cnt+=1
-            if max_area < area:
-                max_area = area
-                area = 1 # 초기화
+            area_list.append(bfs(i, j))
 
-print(cnt)
-print(max_area)
+if len(area_list) > 0 :
+    print(cnt)
+    print(max(area_list))
+else:
+    print(cnt)
+    print(0)
+# print(cnt)
+# print(max(area_list)) # max가 비어있는 리스트에 사용될 경우 런타임 에러가 나온다.
