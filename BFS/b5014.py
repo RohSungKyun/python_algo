3c1ddaaabd480af11136db3e5612221032e86e5e
SUNGKYUN ROH <98266637+RohSungKyun@users.noreply.github.com>
2023-08-17 22:58:41 +0900

    0817

---
+++ BFS/b5014.py
@@ -0,0 +1,24 @@
+# 스타트링크
+from collections import deque
+import sys
+read = sys.stdin.readline
+
+f, s, g, u, d = map(int, read().split())
+visited = [0]*(f+1)
+
+def bfs():
+    queue = deque()
+    queue.append((s))
+    visited[s] = 1
+    while queue:
+        tmp = queue.popleft()
+        for i in [tmp-d, tmp+u]:
+            if 0<=i<f and not visited[i]:
+                visited[i] = visited[tmp]+1
+                queue.append((i))
+
+bfs()
+if visited[g]:
+    print(visited[g]-1)
+else:
+    print('use the stairs')
